e22174e6285373a25ab20b7d6bc43ea8b24037a9
zengjin55 <3289714863@qq.com>
2018-04-17 12:40:54 +0800

    v1.02

---
+++ src/test/java/com/essa/framework/generateReport1.java
@@ -0,0 +1,17 @@
+package com.essa.framework;
+
+import java.util.List;
+
+import org.testng.IReporter;
+import org.testng.ISuite;
+import org.testng.xml.XmlSuite;
+
+public class generateReport1 implements IReporter{
+
+	@Override
+	public void generateReport(List<XmlSuite> arg0, List<ISuite> arg1, String arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
